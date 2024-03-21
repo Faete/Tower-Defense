5b4c9e03ccd3e96e32662222fcb3329c6b5e094b
Matthew Cesena <mattcesena99@gmail.com>
2024-03-21 12:14:40 -0700

    Thingies

---
+++ Assets/Scripts/PortalRotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalRotation : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 8f;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed);
+    }
+}
