5d3fd99d88e9afdc9fc0b799d7d5ca6f91a41fe9
Matthew Cesena <mattcesena99@gmail.com>
2024-03-26 12:03:50 -0700

    Stuff?

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -11,26 +11,36 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] GameObject builderPrefab;
     [SerializeField] Tilemap tilemap;
     [SerializeField] Inventory inventory;
-    [SerializeField] List<GameObject> buttons;
+    [SerializeField] List<GameObject> critterButtons;
+    [SerializeField] GameObject leftButton;
+    [SerializeField] GameObject rightButton;
 
     List<Critter> critterWindow;
     int critterWindowNum = 0;
 
     void Update(){
         SetCritterWindow();
+        DisplayArrowButtons();
     }
 
     void SetCritterWindow(){
-        foreach(GameObject button in buttons){
+        foreach(GameObject button in critterButtons){
             button.SetActive(false);
         }
         critterWindow = new List<Critter>();
         critterWindow.AddRange(inventory.critters.Skip(critterWindowNum * 4).Take(4));
-        for(int i = 0; i < critterWindow.Count(); ++i){
-            buttons[i].SetActive(true);
-            buttons[i].transform.GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = critterWindow[i].sprite;
+        for(int i = 0; i < critterWindow.Count; ++i){
+            critterButtons[i].SetActive(true);
+            critterButtons[i].transform.GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = critterWindow[i].sprite;
         }
 
+    }
+
+    void DisplayArrowButtons(){
+        if((critterWindowNum + 1) * 4 > inventory.critters.Count - 1) rightButton.SetActive(false);
+        else rightButton.SetActive(true);
+        if(critterWindowNum == 0) leftButton.SetActive(false);
+        else leftButton.SetActive(true);
     }
 
     public void Build(int idx){
@@ -41,5 +51,13 @@ public class ButtonManager : MonoBehaviour
         builder.critter = critter;
         builder.critterIdx = critterWindowNum * 4 + idx;
         builder.inventory = inventory;
+    }
+
+    public void IncCritterWindow(){
+        critterWindowNum++;
+    }
+
+    public void DecCritterWindow(){
+        critterWindowNum--;
     }
 }
