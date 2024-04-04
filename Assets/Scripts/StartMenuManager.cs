eecdb6f650f23b966552585d7ac3750e52a3f2cb
Matthew Cesena <mattcesena99@gmail.com>
2024-04-03 22:43:19 -0700

    Implemented Start Menu

---
+++ Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+
+    [SerializeField] Critter gimbo;
+    [SerializeField] GameObject newGameConfirmationCanvas;
+    SaveData savedata;
+    string saveFilePath;
+
+    void Start(){
+        saveFilePath = Application.persistentDataPath + "/critters.json";
+    }
+
+    public void NewGame(){
+        if(!File.Exists(saveFilePath)) NewGameConfirmed();
+        else newGameConfirmationCanvas.SetActive(true);
+    }
+
+    public void NewGameDenied(){
+        newGameConfirmationCanvas.SetActive(false);
+    }
+
+    public void NewGameConfirmed(){
+        savedata = new SaveData();
+        savedata.critters = JsonUtility.ToJson(new SaveableCritter(gimbo));
+        savedata.catchers = 0;
+        savedata.level = 1;
+        string json = JsonUtility.ToJson(savedata);
+        File.WriteAllText(saveFilePath, json);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadGame(){
+        if(System.IO.File.Exists(saveFilePath)) SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+
+}
